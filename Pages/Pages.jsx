0e7b83029b7511502e8e38c219390bc01f989685
Mtalha199 <talhatarar19@gmail.com>
2023-12-18 10:43:10 +0500

    pagesAdded

---
+++ Pages/Pages.jsx
@@ -0,0 +1,16 @@
+
+// import UserPage from "./UserPage";
+import AdminPage from "./AdminPage";
+
+const Pages = () => {
+    return ( 
+        <>
+ <div class="main-content">
+
+ <AdminPage />
+        </div>
+        </>
+     );
+}
+ 
+export default Pages;
